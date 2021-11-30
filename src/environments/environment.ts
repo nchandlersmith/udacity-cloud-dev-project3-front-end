9c2f85c689d58267f0998a97a20699dd3f26f01c
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2021-11-30 10:30:14 -0600

    uses exposed public IP of reverse proxy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ac7aed758817c4b2f9d13cd7145f0321-809537719.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
