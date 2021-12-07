d8a107d3c909d9d2c7348ef4c338aaf1215816be
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2021-12-06 18:51:51 -0600

    moves ports

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-const port = 8080;
+const port = 80;
 const host = 'localhost';
 
 export const environment = {
